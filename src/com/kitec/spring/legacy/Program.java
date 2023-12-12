43ce88166633a670c51b213808442b515454f8c7
baehaeyong <144760352+baehaeyong@users.noreply.github.com>
2023-12-12 17:12:15 +0900

    add Hello

--- src/com/kitec/spring/legacy/Program.java
+++ src/com/kitec/spring/legacy/Program.java
@@ -4,5 +4,6 @@ public class Program {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("remote modification");
+		System.out.println("Hello")
 	}
 }
