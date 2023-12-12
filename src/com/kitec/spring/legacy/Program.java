f9cf6652036f6aa10798ba2863792e3fc3a7f528
Haeyong Bae <baehaeyong@gmail.com>
2023-12-12 10:39:26 +0900

    W -> w

--- src/com/kitec/spring/legacy/Program.java
+++ src/com/kitec/spring/legacy/Program.java
@@ -2,7 +2,7 @@ package com.kitec.spring.legacy;
 
 public class Program {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello world");
 		
 	}
 }
